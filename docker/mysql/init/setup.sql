075fd9606ae35dbe1fe31fe27236b0fca7294893
wozniackia <wozniackia@gmail.com>
2023-06-19 13:42:52 +0200

    migracje

--- docker/mysql/init/setup.sql
+++ docker/mysql/init/setup.sql
@@ -1,3 +1,31 @@
 CREATE DATABASE project;
-CREATE DATABASE test;
-CREATE DATABASE test2;
+
+create table users (
+	id int primary key AUTO_INCREMENT,
+	name varchar(40),
+	email varchar(40),
+	isAdmin boolean default false,
+	password varchar(40),
+	updated_at date,
+	created_at date
+	UNIQUE KEY unique_email (email)  
+);
+
+create table fleet (
+	id int primary key AUTO_INCREMENT,
+	manufacture varchar,
+	model varchar,
+	version varchar,
+	seats int,
+	pricePerHour float,
+	isAvailable boolean
+);
+
+create table orders (
+	id int primary key AUTO_INCREMENT,
+	user_id bigint references users(id),
+	vehicle_id bigint references fleet(id),
+	booked_at date,
+	duration_in_days int,
+	payment_method varchar,
+);
